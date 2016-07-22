5017862fcfcbfb4a1600c0cb1ec30f06666910eb
fyuanfen <fyuanfena@163.com>
2016-07-22 13:36:56 +0800

    first version

--- word.py
+++ word.py
@@ -30,6 +30,7 @@ def processChinese(text):
 
     return seg_list
 
+
 importStopword(filename='./stopwords.txt')
 
 
@@ -38,22 +39,21 @@ importStopword(filename='./stopwords.txt')
 # d = path.dirname('.')
 d = path.dirname(__file__)
 
-
 text = open(path.join(d, 'love.txt'),encoding ='utf-8').read()
 
 #如果是中文
-#text = processChinese(text)#中文不好分词，使用Jieba分词进行
+text = processChinese(text)#中文不好分词，使用Jieba分词进行
 
 # read the mask / color image
 # taken from http://jirkavinse.deviantart.com/art/quot-Real-Life-quot-Alice-282261010
 # 设置背景图片
 back_coloring = imread(path.join(d, "./image/love.jpg"))
 
-wc = WordCloud( font_path='./font/cabin-sketch.bold.ttf',#设置字体
+wc = WordCloud( font_path='./font/叶立群几何体.ttf',#设置字体
                 background_color="black", #背景颜色
                 max_words=2000,# 词云显示的最大词数
                 mask=back_coloring,#设置背景图片
-                max_font_size=100, #字体最大值
+                #max_font_size=100, #字体最大值
                 random_state=42,
                 )
 # 生成词云, 可以用generate输入全部文本(中文不好分词),也可以我们计算好词频后使用generate_from_frequencies函数
